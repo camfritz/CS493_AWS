7a4f5cd8fe1b20c84874ae9330d381ebe6a15e89
Camron Fritz <camfritz@gmail.com>
2019-02-24 18:52:13 -0800

    Change S3 CLI tool to insert items into ddb.

--- s3_tool.py
+++ s3_tool.py
@@ -24,6 +24,14 @@ s3 = boto3.resource(
     aws_session_token=credentials['SessionToken'],
 )
 
+ddb = boto3.resource(
+	'dynamodb',
+	aws_access_key_id=credentials['AccessKeyId'],
+	aws_secret_access_key=credentials['SecretAccessKey'],
+	aws_session_token=credentials['SessionToken'],
+	)
+
+table = ddb.Table('music')
 
 ###############################################################
 
@@ -57,17 +65,39 @@ if(os.path.isfile(upload_path)):
 		upload_name = raw_input('Enter upload name of file. (blank = original)')
 	upload_target = path_leaf(upload_path)
 
+	artist_name = raw_input('Enter name of artist')
+	album_name = raw_input('Enter name of album')
+	genre_name = raw_input('Enter name of genre')
+
 	if(upload_name == ''):
-		# s3.meta.client.upload_file(str(upload_path), 'cf-privatebucket01', 'songs/' + str(upload_target))
 		s3.Object('cf-privatebucket01', 'songs/' + str(upload_target)).upload_file(Filename=str(upload_path))
+
+		table.put_item(
+			Item={
+				'genre' : genre_name,
+				'artist_album_song' : artist_name + '#' + album_name + '#' + upload_target[:-4],
+				'artist' : artist_name
+			})
 	else:
-		# s3.meta.client.upload_file(str(upload_path), 'cf-privatebucket01', 'songs/' + str(upload_name))
 		s3.Object('cf-privatebucket01', 'songs/' + str(upload_name)).upload_file(Filename=str(upload_path))
 
+		table.put_item(
+			Item={
+				'genre' : genre_name,
+				'artist_album_song' : artist_name + '#' + album_name + '#' + upload_name[:-4],
+				'artist' : artist_name
+			})
+
+
 elif(os.path.isdir(upload_path)):
+	artist_name = ''
 	#if path is a folder, upload all objects in folder to s3 with option to rename folder and/or storage path
 	if(len(sys.argv) <= 1):
 		upload_name = raw_input('Enter upload name of album/artist. (blank = original)')
+
+	if(upload_name == ''):
+		artist_name = raw_input('Enter name of artist')
+	genre_name = raw_input('Enter name of genre')
 
 	rootTrigger = True;
 
@@ -79,10 +109,40 @@ elif(os.path.isdir(upload_path)):
 		for file in files:
 			if(upload_name == ''):
 				#s3.meta.client.upload_file(os.path.join(root, file), 'cf-privatebucket01', os.path.join(root[len(firstHeadName) + 1:], file))
+				string = (root[len(firstHeadName) + 1:]).split('/')
 				s3.Object('cf-privatebucket01', os.path.join(root[len(firstHeadName) + 1:], file)).upload_file(Filename=os.path.join(root, file))
+
+				table.put_item(
+					Item={
+						'genre' : genre_name,
+						'artist_album_song' : artist_name + '#' + string[len(string) - 1] + '#' + file[:-4],
+						'artist' : artist_name
+					})
+
 			else:
 				#s3.meta.client.upload_file(os.path.join(root, file), 'cf-privatebucket01', str(upload_name + '/' + os.path.join(root[len(firstHeadName) + len(firstRootName) + 2:], file)))
 				s3.Object('cf-privatebucket01', str(upload_name + '/' + os.path.join(root[len(firstHeadName) + len(firstRootName) + 2:], file))).upload_file(Filename=os.path.join(root, file))
+
+				string = (root[len(firstHeadName) + 1:]).split('/')
+
+				if(len(string) >= 2):
+					table.put_item(
+						Item={
+						'genre' : genre_name,
+						'artist_album_song' : upload_name + '#' + string[len(string) - 1] + '#' + file[:-4],
+						'artist' : upload_name
+						})
+				else:
+					if(artist_name == ''):
+						artist_name = raw_input('Enter name of artist')
+
+					table.put_item(
+							Item={
+								'genre' : genre_name,
+								'artist_album_song' : artist_name + '#' + upload_name + '#' + file[:-4],
+								'artist' : artist_name
+							}
+						)
 else:
 	print('invalid path or file does not exist')
 
